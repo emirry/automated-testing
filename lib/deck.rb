5f276930ce46a1ee0b630678d2600072b30682a8
Emily Nagai <emnagai89@gmail.com>
2020-09-28 19:55:15 -0700

    Incomplete. Turning in what I have.

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,9 +4,15 @@
 require_relative 'card'
 
 class Deck
+  attr_reader :cards
 
   def initialize
-
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        cards.push(Card.new(value, suit))
+      end
+    end
   end
 
   def draw
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,5 +5,20 @@ describe Deck do
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
+    describe "A deck holds 52 valid cards" do
+      it "can be instantiated" do
+        deck = Deck.new
 
+        expect(deck).must_be_instance_of Deck
+      end
+    end
+
+
+  # describe "test that draw removes cards from the deck and returns a hand" do
+  #
+  # end
+  #
+  # describe "test that count returns the hand back into the deck" do
+  #
+  # end
 end
